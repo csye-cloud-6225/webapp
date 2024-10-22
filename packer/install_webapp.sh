dfe6006940378286032252c7aca71b32b03caa52
Nilvi Shah <shah.nilv@northeastern.edu>
2024-10-21 23:40:57 -0400

    Creating ami without mysql configuration

--- packer/install_webapp.sh
+++ packer/install_webapp.sh
@@ -11,37 +11,37 @@ log_message "checking worklow file"
 # Step 1: Update packages and install dependencies
 log_message "Step 1: Updating packages and installing dependencies..."
 sudo apt-get update
-sudo apt-get install -y nodejs npm unzip mysql-server
+# sudo apt-get install -y nodejs npm unzip mysql-server
 
-# Step 2: Configure MySQL server
-log_message "Configuring MySQL server..."
+# # Step 2: Configure MySQL server
+# log_message "Configuring MySQL server..."
 
-# Enable and start MySQL service
-log_message "Starting MySQL service..."
-sudo systemctl enable mysql
-sudo systemctl start mysql
+# # Enable and start MySQL service
+# log_message "Starting MySQL service..."
+# sudo systemctl enable mysql
+# sudo systemctl start mysql
 
-# Configure MySQL
-log_message "Configuring MySQL..."
-sudo mysql -u root -p'Parna.coM001'<<EOF
-CREATE DATABASE IF NOT EXISTS health_check;
-SELECT user, host, plugin FROM mysql.user WHERE user = 'root';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Parna.coM001';
-FLUSH PRIVILEGES;
-SHOW DATABASES;
-SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'health_check';
-EOF
+# # Configure MySQL
+# log_message "Configuring MySQL..."
+# sudo mysql -u root -p'Parna.coM001'<<EOF
+# CREATE DATABASE IF NOT EXISTS health_check;
+# SELECT user, host, plugin FROM mysql.user WHERE user = 'root';
+# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Parna.coM001';
+# FLUSH PRIVILEGES;
+# SHOW DATABASES;
+# SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'health_check';
+# EOF
 
-log_message "MySQL configuration completed."
+# log_message "MySQL configuration completed."
 
-# Verify MySQL connection
-log_message "Verifying MySQL connection..."
-if mysql -u root -p'Parna.coM001' -e "SHOW DATABASES;" > /dev/null 2>&1; then
-    log_message "MySQL connection successful."
-else
-    log_message "Error: Unable to connect to MySQL. Check the error log for details."
-    exit 1
-fi
+# # Verify MySQL connection
+# log_message "Verifying MySQL connection..."
+# if mysql -u root -p'Parna.coM001' -e "SHOW DATABASES;" > /dev/null 2>&1; then
+#     log_message "MySQL connection successful."
+# else
+#     log_message "Error: Unable to connect to MySQL. Check the error log for details."
+#     exit 1
+# fi
 # sudo mysql -u root <<EOF
 # ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Parna.coM001';
 # FLUSH PRIVILEGES;
@@ -89,7 +89,7 @@ log_message "Installing Node.js dependencies..."
 sudo -u csye6225 bash -c 'cd /opt/webapp && npm install'
 sudo npm uninstall bcrypt
 sudo npm install bcrypt
-sudo -u csye6225 npx sequelize-cli db:migrate
+# sudo -u csye6225 npx sequelize-cli db:migrate
 
 echo "Contents of .env file:"
 cat /opt/webapp/.env
