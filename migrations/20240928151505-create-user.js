47182e53d109304dd98e254ecafcb42ca62fe308
Nilvi Shah <shah.nilv@northeastern.edu>
2024-10-29 10:51:11 -0400

    resolving the tests error because of 500 error

--- migrations/20240928151505-create-user.js
+++ migrations/20240928151505-create-user.js
@@ -39,13 +39,17 @@ module.exports = {
         allowNull: false,
         defaultValue: Sequelize.NOW // Automatically set when user is created, updated with hooks
       },
-      fileName: {
+      profilePicUrl: {
         type: Sequelize.STRING, // Add the fileName field
         allowNull: true, // Optional field
       },
-      url: {
-        type: Sequelize.STRING, // Add the url field
+      profilePicUploadedAt: {
+        type: Sequelize.DATE, // Add the url field
         allowNull: true, // Optional field
+      },
+      profilePicOriginalName: {
+        type: DataTypes.STRING,
+        allowNull: true, // Allow null if no profile picture is uploaded
       },
       createdAt: {
         allowNull: false,
