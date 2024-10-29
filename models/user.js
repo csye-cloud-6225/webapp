e821462ef3f5f76c4d8bd3ced010d5aa500bdcad
Nilvi Shah <shah.nilv@northeastern.edu>
2024-10-29 19:40:08 -0400

    error

--- models/user.js
+++ models/user.js
@@ -1,62 +1,58 @@
 'use strict';
-const { Model } = require('sequelize');
-
-module.exports = (sequelize, DataTypes) => {
-  class Image extends Model {
-    static associate(models) {
-      // Each image belongs to a single user
-      Image.belongsTo(models.User, {
-        foreignKey: 'userId', // This is the foreign key
-        as: 'user', // Optional alias for easier access
-      });
-    }
-  }
-
-  Image.init({
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
-    },
-    userId: {
-      type: DataTypes.UUID, // Foreign key to match the User table
-      allowNull: false,
-      references: {
-        model: 'users', // Ensure this matches the casing of your Users table
-        key: 'id',
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID, // Use UUID instead of INTEGER
+        defaultValue: Sequelize.UUIDV4, // Automatically generate UUIDs
+        allowNull: false,
+        primaryKey: true,
       },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    },
-    profilePicUrl: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    profilePicOriginalName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    profilePicUploadedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false,
-    },
-  }, {
-    sequelize,
-    modelName: 'Image',
-    tableName: 'images',
-  });
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true, // Ensures email is unique
+        validate: {
+          isEmail: true, // Ensures valid email format
+        },
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      account_created: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      account_updated: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
 
-  return Image;
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
 };
